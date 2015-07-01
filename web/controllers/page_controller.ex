1a74688bf6af1d41781d7c52cd96602179eaa192
Haukur Páll Hallvarðsson <hph@hph.is>
2015-07-01 03:14:11 -0300

    Add default layout; use CDNs for vendor assets
    It is preferable to use CDNs or external sources in general for vendor assets.
    Phoenix.js is part of the framework but as it's not bundled with it, it is
    better to use a CDN than keeping the code with our custom code. This way it's
    easier to maintain the whole app and make updates when required. The file
    shouldn't be in our Git history at all.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,6 +4,6 @@ defmodule Xadrez.PageController do
   plug :action
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, :index
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -8,18 +8,9 @@ defmodule Xadrez.Router do
     plug :protect_from_forgery
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", Xadrez do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Xadrez do
-  #   pipe_through :api
-  # end
 end
