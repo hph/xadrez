84cecb3732592fad0de851fb82a7b997178635d3
Haukur Páll Hallvarðsson <hph@hph.is>
2015-07-04 02:11:09 -0300

    Add User model

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule Xadrez.UserTest do
+  use Xadrez.ModelCase
+
+  alias Xadrez.User
+
+  @valid_attrs %{email: "some content", password: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
